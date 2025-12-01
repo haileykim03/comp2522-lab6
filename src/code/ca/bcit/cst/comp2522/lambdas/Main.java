8f3c05085bb0723c26c0c59a79ff2e6b37e73495
haileykim03 <skim682@my.bcit.ca>
2025-12-01 15:23:12 -0800

    change imports

--- src/code/ca/bcit/cst/comp2522/lambdas/Main.java
+++ src/code/ca/bcit/cst/comp2522/lambdas/Main.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.function.Supplier;
 
 /**
  * Demonstrates the use of the HockeyPlayer and HockeyTeam classes
